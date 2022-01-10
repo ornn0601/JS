3dc1853fded4e009a242c404e401f0be80b8b91a
euitae PARK <57550670+ornn0601@users.noreply.github.com>
2022-01-10 15:05:06 +0900

    주석 추가
    주석 추가

--- click_toggle_action.js
+++ click_toggle_action.js
@@ -2,11 +2,11 @@
 // Jquery 토글
 jQuery(document).ready(function ($) {
 
-	// 해당 
+	// 해당 요소 담기
     let titleBtn = $('버튼');
     let contentBox = $('콘텐츠');	
 
-	// 버튼을 클릭하면
+	// 버튼을 클릭하면 class와 슬라이드 액션 적용
     titleBtn.on('click', function () {
         $(this).toggleClass('active'); // 해당 버튼에 클래스 'active' on/off
         contentBox.slideToggle(); // 콘텐츠 나타나는 액션을 슬라이드 적용, 속도 () 기본값 적용
